0aa1e48967ca75d279536c75cfd4baa89f06026b
jimmy.nguyen <tuannguyen@2fsoft.info>
2024-01-09 20:09:22 +0700

    fixed verify token after logged in

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,14 +3,23 @@ const {FORBIDDEN, UNAUTHORIZED} = require("../utils/status_code.util");
 
 const auth = (req, res, next) => {
   try {
-    const token = req.header("Authorization");
+    let token = req.header('Authorization');
     if (!token) {
       return res.status(FORBIDDEN).json({
         error: "Access denied. Seem like you haven't provided Authorization token, yet."
       });
     }
 
-    req.user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    token = token.split(' ');
+
+    if (token.length !== 2) {
+      return res.status(FORBIDDEN).json({
+        error: "Token is invalid.",
+      });
+    }
+
+    const accessToken = token[1];
+    req.user = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
     next();
   } catch (error) {
     res.status(UNAUTHORIZED).json({
